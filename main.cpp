714d731fc5272726146e96a9206ea24f69d0a419
Simon Coenen <sim2.coenen@gmail.com>
2025-10-18 12:27:09 +0200

    Add more instrumentation

--- main.cpp
+++ main.cpp
@@ -226,16 +226,21 @@ int main(int, char**)
     {
         PROFILE_FRAME();
 
-        // Poll and handle messages (inputs, window resize, etc.)
-        // See the WndProc() function below for our to dispatch events to the Win32 backend.
-        MSG msg;
-        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
         {
-            ::TranslateMessage(&msg);
-            ::DispatchMessage(&msg);
-            if (msg.message == WM_QUIT)
-                done = true;
-        }
+			PROFILE_CPU_SCOPE("WndProc");
+
+			// Poll and handle messages (inputs, window resize, etc.)
+			// See the WndProc() function below for our to dispatch events to the Win32 backend.
+			MSG msg;
+			while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+			{
+				::TranslateMessage(&msg);
+				::DispatchMessage(&msg);
+				if (msg.message == WM_QUIT)
+					done = true;
+			}
+		}
+
         if (done)
             break;
 
@@ -568,6 +573,8 @@ void WaitForPendingOperations()
 
 FrameContext* WaitForNextFrameContext()
 {
+	PROFILE_CPU_SCOPE();
+
     FrameContext* frame_context = &g_frameContext[g_frameIndex % APP_NUM_FRAMES_IN_FLIGHT];
     if (g_fence->GetCompletedValue() < frame_context->FenceValue)
     {
